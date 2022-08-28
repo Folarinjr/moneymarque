aa0ad03eed66bb94e1f4ad8a50211dc9921576b8
Folarinjr <folarinademola2@gmail.com>
2022-08-28 02:45:47 +0100

    Navigation bar successfully added

--- App.js
+++ App.js
@@ -1,9 +1,56 @@
-import MainContainer from './navigation/MainContainer'
+import { StatusBar } from 'expo-status-bar';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {Ionicons, MaterialCommunityIcons} from '@expo/vector-icons/';
+
+import HomeScreen from './navigation/screens/HomeScreen';
+import TargetScreen from './navigation/screens/TargetScreen';
+import ProfileScreen from './navigation/screens/ProfileScreen'
+
+const Tab = createMaterialBottomTabNavigator();
 
 const App= () => {
   return (
-    <MainContainer/>
+    <NavigationContainer>
+      <StatusBar style='auto'/>
+     <Tab.Navigator
+        initialRouteName='home'
+        labeled={false} barStyle={{ backgroundColor: '#ebe6e6' }} 
+        activeColor="#427ddb"
+      >
+        <Tab.Screen 
+          name='home' 
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={30}/>
+            ),
+          }} 
+        />
+        <Tab.Screen 
+          name='target' 
+          component={TargetScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="target" color={color} size={30}/>
+            ),
+          }} 
+        />
+        <Tab.Screen 
+          name='person' 
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={30}/>
+            ),
+          }} 
+        />
+     </Tab.Navigator>
+    </NavigationContainer>
   );
 }
+
 
 export default App;
---
+++ navigation/screens/TargetScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const TargetScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+      }}
+    >
+      <Text>TargetScreen</Text>
+    </View>
+  )
+}
+
+export default TargetScreen
